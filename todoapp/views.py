89434e25c650711d61dc43edcdb88e2bd9131eae
andreeapaul95 <andreeapaul@protonmail.com>
2021-06-27 18:49:04 +0300

    Add AddTsk Form

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def home(request):
-    todos = Todo.objects.all()
-    return render(request, 'home.html', {'todos': todos})
+    if request.method == 'POST':
+        form = TodoForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            todos = Todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
+    else:
+        todos = Todo.objects.all()
+        return render(request, 'home.html', {'todos': todos})
