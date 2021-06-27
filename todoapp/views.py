ab70b6fb4b9fe90a41debca42f43590f5e1fe088
andreeapaul95 <andreeapaul@protonmail.com>
2021-06-27 18:54:27 +0300

    Add messages

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Todo
 from .forms import TodoForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -10,6 +11,7 @@ def home(request):
         if form.is_valid():
             form.save()
             todos = Todo.objects.all()
+            messages.success(request, ('Task has been added!'))
             return render(request, 'home.html', {'todos': todos})
     else:
         todos = Todo.objects.all()
