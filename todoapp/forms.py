437f90cfa88e0ea18c9d7667e4357d9b77d645d9
andreeapaul95 <andreeapaul@protonmail.com>
2021-08-01 14:24:31 +0300

    Add a due date for task

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from django.forms import widgets
+from django.forms.fields import TimeField
 from .models import Todo
+
+
+class DateImput(forms.DateInput):
+    input_type = "date"
+
+
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ['task', 'completed', 'category']
+        fields = ['task', 'completed', 'category', 'due_date']
+        widgets = {
+            'due_date': DateImput()
+        }
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
--- todoapp/models.py
+++ todoapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Category(models.Model):
-    name = models.ChoiceField(max_length=200) 
+    name = models.CharField(max_length=200) 
+
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
 
     def __str__(self):
         return self.name  
@@ -15,6 +20,9 @@ class Todo(models.Model):
     user  = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    due_date = models.DateField(null=True, blank= True)
+    
+
 
     class Meta:
         ordering = ("-created", )
